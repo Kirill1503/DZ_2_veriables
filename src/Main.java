eeeaa8c7734c50565d6f0d829c8aa6f8e8821a87
Kirill <K.katyshev1503@gmail.com>
2022-05-20 17:57:13 +0300

    выполнил первое дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+public class Task {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a = 5;
+        System.out.println(a);
+        short b = 320;
+        System.out.println(b);
+        int c = 349000;
+        System.out.println(c);
+        long d = 475849302L;
+        System.out.println(d);
+        float e = 7.23921f;
+        System.out.println(e);
+        double f = 21.5885949303;
+        System.out.println(f);
+    }
+}
+class Task_2 {
+    public static void main(String[] args) {
+
     }
 }
